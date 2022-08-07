9bb1ed77f84db2cd1c64bcb18cfa679cad36d52b
ACimensel <cimensel.a@hotmail.com>
2022-08-07 01:56:58 -0700

    Created a game window and can move a single cube around the game screen

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import socket
 
 class Client:
@@ -21,6 +22,44 @@ class Client:
         except socket.error as e:
             return str(e)
 
+def draw_block():
+    surface.fill((255, 176, 227))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
 # game logic
 # probably need Grid, Snake, Apple, and some other classes
 # run game
+if __name__ == '__main__':
+    pygame.init()
+
+    surface = pygame.display.set_mode((800, 800))
+    surface.fill((255, 176, 227))
+
+    block = pygame.image.load("resources/block.jpg").convert()
+    block_x, block_y = 0, 0
+
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip() # refresh UI window
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                if event.key == K_UP:
+                    block_y -= 40
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 40
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 40
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 40
+                    draw_block()
+
